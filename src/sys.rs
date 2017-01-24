b6f91bd8b8eeb07d48a9a7bb547a94e00a56ba33
Evgeny Safronov <division494@gmail.com>
2017-01-24 19:55:50 +0300

    fix: crossplatform

--- src/sys.rs
+++ src/sys.rs
@@ -5,11 +5,16 @@ use libc;
 use nix::sys::socket;
 
 pub fn sendmsg(fd: RawFd, iov: &[&[u8]]) -> Result<usize, Error> {
+    #[cfg(target_os = "linux")]
+    let len = iov.len() as usize;
+    #[cfg(target_os = "macos")]
+    let len = iov.len() as i32;
+
     let mhdr = libc::msghdr {
         msg_name: 0 as *mut libc::c_void,
         msg_namelen: 0,
         msg_iov: iov.as_ptr() as *mut libc::iovec,
-        msg_iovlen: iov.len() as i32,
+        msg_iovlen: len,
         msg_control: 0 as *mut libc::c_void,
         msg_controllen: 0,
         msg_flags: 0,
