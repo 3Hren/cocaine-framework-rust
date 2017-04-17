12f224fcf8eed2a6e611376955de4dbc8d746c4d
Evgeny Safronov <division494@gmail.com>
2017-04-17 19:15:24 +0300

    feat: more examples

--- examples/locator.rs
+++ examples/locator.rs
@@ -3,7 +3,7 @@ extern crate futures;
 extern crate tokio_core;
 extern crate rmpv;
 
-use futures::{future, Stream};
+use futures::{future, Future, Stream};
 use tokio_core::reactor::Core;
 
 use cocaine::{Service};
@@ -13,10 +13,15 @@ fn main() {
     let mut core = Core::new().unwrap();
     let locator = Locator::new(Service::new("locator", &core.handle()));
 
+    let future = locator.resolve("node").and_then(|info| {
+        println!("{:?}", info);
+        future::ok(())
+    });
+    core.run(future).unwrap();
+
     let stream = locator.routing("whatever").for_each(|chunk| {
         println!("{:?}", chunk);
         future::ok(())
     });
-
     core.run(stream).unwrap();
 }
