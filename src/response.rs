724ab858f9f294834ee05aff1f24437d6fa60b3d
Evgeny Safronov <division494@gmail.com>
2017-10-27 17:35:40 +0300

    fix: warnings

--- src/response.rs
+++ src/response.rs
@@ -38,7 +38,7 @@ pub struct Response<'a: 'b, 'b> {
 }
 
 impl<'a: 'b, 'b> Response<'a, 'b> {
-    pub(crate) fn new(ty: u64, args: &'b ValueRef<'a>, meta: &'b ValueRef<'a>) -> Result<Self, rmpv::ext::Error> {
+    pub(crate) fn new(ty: u64, args: &'b ValueRef<'a>, _meta: &'b ValueRef<'a>) -> Result<Self, rmpv::ext::Error> {
         let resp = Self {
             ty: ty,
             args: args,
